12946f0c792ae28f2ec0b2ebc959d1af98540d4c
Yash Jagdale <jagdale0210@gmail.com>
2023-02-16 22:23:38 +0530

    Update errors.go

--- errors.go
+++ errors.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrConstraintsNotImplemented = errors.New("constraints not implemented on sqlite, consider using DisableForeignKeyConstraintWhenMigrating, more details https://github.com/go-gorm/gorm/wiki/GORM-V2-Release-Note-Draft#all-new-migrator")
+	ErrSQLiteBusy = errors.New("database is locked (5) (SQLITE_BUSY)")
 )
